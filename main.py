db95359ca3a443402244fa938d2a13e8a9e627e5
Rey Benedicto <reybenedicto06@gmail.com>
2023-12-26 01:10:14 +0800

    All skills now have actual outputs, completed end-to-end flow

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from util.ResourceFactory import ResourceFactory
 def main():
     company = WorkspaceManager().get_company("Calcutech")
     company_manager = CompanyManager(company)
-    company_manager.receive_resource(ResourceFactory.create("Prompt", "Trending topic right now on social media"))
+    company_manager.receive_resource(ResourceFactory.create("Prompt", "ChatGPT"))
 
 
 main()
